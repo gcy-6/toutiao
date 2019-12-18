2cc2b739ad080bf582a1665c68e85cddd36d3599
gcy <gcy4399@163.com>
2019-12-18 22:41:37 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,59 +4,60 @@
               <div class='title'>
                   <img src="../../assets/img/logo_index.png" alt="">
               </div>
-              <el-form style="margin-top:20px">
-                  <el-form-item>
-                      <el-input placeholder="请输入您的手机号"></el-input>
+              <el-form style="margin-top:20px" :model='loginForm' :rules='loginRules' ref="myForm">
+                  <el-form-item prop='mobile'>
+                      <el-input v-model="loginForm.mobile" placeholder="请输入您的手机号"></el-input>
                   </el-form-item>
-                  <el-form-item>
-                      <el-input style="width:65%"  placeholder="请输入验证码"></el-input>
+                  <el-form-item prop='code'>
+                      <el-input style="width:65%" v-model="loginForm.code" placeholder="请输入验证码"></el-input>
                       <el-button plain style="float:right">发送验证码</el-button>
+                  </el-form-item >
+                  <el-form-item prop='check'>
+                      <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
                   </el-form-item>
                   <el-form-item>
-                      <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
-                  </el-form-item>
-                  <el-form-item>
-                      <el-button type="primary" style="width:100%">登录</el-button>
+                      <el-button @click='submitLogin' type="primary" style="width:100%">登录</el-button>
                   </el-form-item>
               </el-form>
 
           </el-card>
       </div>
 
-      <!-- <div class='login'>
-
-     <el-card class='login-card'>
-       <div class='title'>
-         <img src="../../assets/img/logo_index.png" alt="">
-       </div>
-
-       <el-form style="margin-top:30px">
-
-         <el-form-item>
-
-           <el-input placeHolder="请输入手机号"></el-input>
-         </el-form-item>
-         <el-form-item>
-
-           <el-input style="width:65%" placeHolder="请输入验证码"> </el-input>
-           <el-button style="float:right" plain>发送验证码</el-button>
-         </el-form-item>
-         <el-form-item>
-           <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
-         </el-form-item>
-         <el-form-item>
-           <el-button type="primary" style="width:100%">登录</el-button>
-         </el-form-item>
-       </el-form>
-     </el-card>
-  </div> -->
-
 </template>
 
 <script>
 
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '请输入您的手机号' }, { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }],
+        code: [{ required: true, message: '请输入正确的验证码' }, { pattern: /^\d{6}$/, message: '验证码为6位' }],
+        // 自定义校验函数
+        // value 我们要校验的字段的值
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('请先勾选'))
+          }
+        } }]
 
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myForm.validate(function (isOK) {
+        console.log('请先调用接口')
+      })
+    }
+  }
 }
 </script>
 
