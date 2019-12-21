5906a1d71c7324da5aa810917a297846e11cd3a3
gcy <gcy4399@163.com>
2019-12-21 20:27:26 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import LayoutAside from './home/layout-aside'
 import LayoutHeader from './home/layout-header'
+import BreadCrumb from '../components/common'
 
 export default {
   install (Vue) {
     Vue.component('layout-aside', LayoutAside)
     Vue.component('layout-header', LayoutHeader)
+    Vue.component('bread-crumb', BreadCrumb)
   }
 }
