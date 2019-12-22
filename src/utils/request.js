189dff08a0b0d238cd208f33ca3291823913b35e
gcy <gcy4399@163.com>
2019-12-22 17:07:06 +0800

    统一处理大数字类型

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import router from '../router'
 import { Message } from 'element-ui'
+import JsonBigInt from 'json-bigint'
 
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 // 请求拦截器
@@ -13,7 +14,9 @@ axios.interceptors.request.use(function (config) {
   return config
 }, function () { // 请求失败
 })
-
+axios.defaults.transformResponse = [function (data) {
+  return JsonBigInt.parse(data)
+}]
 // 响应拦截
 axios.interceptors.response.use(function (response) {
   // 响应成功
