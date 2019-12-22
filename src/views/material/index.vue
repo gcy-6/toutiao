539a31f8e3b75b42234c218e893ca3321bea946b
gcy <gcy4399@163.com>
2019-12-22 18:01:46 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,11 +3,23 @@
       <bread-crumb slot="header">
         <template slot="title">素材管理</template>
       </bread-crumb>
+      <el-tabs v-model="activeName">
+          <el-tab-pane label="全部" name='all'>122</el-tab-pane>
+          <el-tab-pane label="收藏" name='collect'>123</el-tab-pane>
+      </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeName: 'collect'
+    }
+  },
+  methods: {
+
+  }
 
 }
 </script>
