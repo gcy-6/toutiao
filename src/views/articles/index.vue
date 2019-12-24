01a7b8a3c66a911c6f9e250951b811f0c210fb18
gcy <gcy4399@163.com>
2019-12-24 19:50:57 +0800

    ## 内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,17 +1,83 @@
 <template>
-  <el-card>
-      <bread-crumb slot="header">
-        <template slot="title">内容列表</template>
-      </bread-crumb>
+  <el-card class="articles">
+    <bread-crumb slot="header">
+      <template slot="title">内容列表</template>
+    </bread-crumb>
+    <el-row class="searchTool">
+      <el-col :span="2">
+        <span>文章状态:</span>
+      </el-col>
+      <el-col :span="18">
+        <el-radio-group v-model="formData.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
+        </el-radio-group>
+      </el-col>
+    </el-row>
+    <el-row class="searchTool">
+      <el-col :span="2">
+        <span>频道列表:</span>
+      </el-col>
+      <el-col :span="18">
+        <el-select placeholder="请选择" v-model="formData.channels_id">
+          <el-option v-for="item in channels" :key="item.id"
+          :label="item.name"
+          :value="item.id"></el-option>
+        </el-select>
+      </el-col>
+    </el-row>
+    <el-row class="searchTool">
+      <el-col :span="2">
+        <span>时间选择:</span>
+      </el-col>
+      <el-col :span="18">
+        <el-date-picker
+          v-model="formData.dataRange"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        status: 5,
+        channels_id: null,
+        dateRange: []
+      },
+      channels: []
+    }
+  },
+  methods: {
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.articles {
+  .searchTool {
+    height: 60px;
+    margin-left: 50px;
+  }
+}
 </style>
