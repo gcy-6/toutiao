eef1e0d2e0c47b41c7e1932b60bc7d5ec5daba29
gcy <gcy4399@163.com>
2019-12-24 23:09:03 +0800

    内容列表-页面结构-搜索筛选

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -8,7 +8,7 @@
         <span>文章状态:</span>
       </el-col>
       <el-col :span="18">
-        <el-radio-group v-model="formData.status">
+        <el-radio-group v-model="formData.status" @change="changeCondition">
           <el-radio :label="5">全部</el-radio>
           <el-radio :label="0">草稿</el-radio>
           <el-radio :label="1">待审核</el-radio>
@@ -22,7 +22,7 @@
         <span>频道列表:</span>
       </el-col>
       <el-col :span="18">
-        <el-select placeholder="请选择" v-model="formData.channels_id">
+        <el-select @change="changeCondition" placeholder="请选择" v-model="formData.channels_id">
           <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-col>
@@ -33,7 +33,9 @@
       </el-col>
       <el-col :span="18">
         <el-date-picker
-          v-model="formData.dataRange"
+          @change="changeCondition"
+          value-format="yyyy-MM-dd"
+          v-model="formData.dateRange"
           type="daterange"
           range-separator="-"
           start-placeholder="开始日期"
@@ -50,14 +52,14 @@
       <el-col :span="14">
         <el-row type="flex">
           <!-- <el-col :span="4"> -->
-            <img :src="item.cover.images.length?item.cover.images[0]:defaultImg" alt />
+          <img :src="item.cover.images.length?item.cover.images[0]:defaultImg" alt />
           <!-- </el-col> -->
           <!-- <el-col :span="9"> -->
-            <div class="info">
-              <p>{{item.title}}</p>
-              <el-tag class="tag" :type="item.status|filterType">{{item.status|filterStatus}}</el-tag>
-              <p class="date">{{item.pubdate}}</p>
-            </div>
+          <div class="info">
+            <p>{{item.title}}</p>
+            <el-tag class="tag" :type="item.status|filterType">{{item.status|filterStatus}}</el-tag>
+            <p class="date">{{item.pubdate}}</p>
+          </div>
           <!-- </el-col> -->
         </el-row>
       </el-col>
@@ -120,6 +122,29 @@ export default {
     }
   },
   methods: {
+    changeCondition () {
+      let params = {
+        status: this.formData.status === 5 ? null : this.formData.status,
+        channel_id: this.formData.channels_id,
+        begin_pubdate: this.formData.dateRange.length
+          ? this.formData.dateRange[0]
+          : null,
+        end_pubdate:
+          this.formData.dateRange.length > 1 ? this.formData.dateRange[1] : null
+      }
+      this.getArticles(params)
+    },
+    // changeCondition () {
+    //   // 组装条件
+    //   // 最新状态
+    //   let params = {
+    //     status: this.formData.status === 5 ? null : this.formData.status, // 不传为全部 5代表全部
+    //     channel_id: this.formData.channel_id, // 频道
+    //     begin_pubdate: this.formData.dateRange.length ? this.formData.dateRange[0] : null, // 起始时间
+    //     end_pubdate: this.formData.dateRange.length > 1 ? this.formData.dateRange[1] : null // 截止时间
+    //   }
+    //   this.getArticles(params) // 调用获取文章数据
+    // },
     getChannels () {
       this.$axios({
         url: '/channels'
@@ -127,9 +152,10 @@ export default {
         this.channels = result.data.channels
       })
     },
-    getArticles () {
+    getArticles (params) {
       this.$axios({
-        url: '/articles'
+        url: '/articles',
+        params
       }).then(result => {
         this.list = result.data.results
       })
@@ -150,19 +176,19 @@ export default {
   }
 }
 .total {
-    border-bottom: 1px dashed #ccc;
-    padding: 10px 0;
+  border-bottom: 1px dashed #ccc;
+  padding: 10px 0;
 }
 
 .article-item {
-    padding: 20px;
-    border-bottom: 1px solid #f2f3f5;
-    img {
-        width: 150px;
-        height: 140px;
-    }
-    .info {
-        margin-left: 25px
-    }
+  padding: 20px;
+  border-bottom: 1px solid #f2f3f5;
+  img {
+    width: 150px;
+    height: 140px;
+  }
+  .info {
+    margin-left: 25px;
+  }
 }
 </style>
