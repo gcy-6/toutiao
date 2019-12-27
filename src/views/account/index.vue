b7ed5cc821f6828083455192bb5887f422bcf40d
gcy <gcy4399@163.com>
2019-12-28 00:22:21 +0800

    账户信息-头像上传更新

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <el-card>
+  <el-card v-loading="loading">
       <bread-crumb slot='header'>
          <template slot='title'>账户信息</template>
       </bread-crumb>
        <!-- 放置上传组件 -->
-      <el-upload class='head-upload' action="" :show-file-list="false">
+      <el-upload :http-request="uploadImg" class='head-upload' action="" :show-file-list="false">
           <img :src="formData.photo?formData.photo:defaultImg" alt="">
       </el-upload>
       <!-- 放置组件 -->
@@ -32,6 +32,7 @@
 export default {
   data () {
     return {
+      loading: false,
       defaultImg: require('../../assets/img/avatar.jpg'),
       // 定义一个表单数据对象
       formData: {
@@ -56,6 +57,20 @@ export default {
     }
   },
   methods: {
+    //   上传图片
+    uploadImg (params) {
+      this.loading = true // 打开弹层
+      let data = new FormData() // 实例化对象
+      data.append('photo', params.file) // 加入参数
+      this.$axios({
+        url: '/user/photo',
+        method: 'patch',
+        data
+      }).then(result => {
+        this.formData.photo = result.data.photo // 设置头像地址
+        this.loading = false // 关调弹层
+      })
+    },
     saveUserInfo () {
       // 要去校验表单数据是否OK
       this.$refs.myForm.validate((isOK) => {
