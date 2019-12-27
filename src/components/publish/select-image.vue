9e999c75a620e9034cc22b74c1931ff2ad423334
gcy <gcy4399@163.com>
2019-12-27 23:59:03 +0800

    发布文章-封面图片选择组件上传

--- src/components/publish/select-image.vue
+++ src/components/publish/select-image.vue
@@ -16,7 +16,13 @@
       </el-row>
 
     </el-tab-pane>
-    <el-tab-pane label="上传" name="upload">上传</el-tab-pane>
+    <!-- <el-tab-pane label="上传" name="upload">上传</el-tab-pane> -->
+    <el-tab-pane label="上传图片" name="upload">
+        <!-- 放置一个上传组件 -->
+        <el-upload class='upload' :http-request="uploadImg" action="" :show-file-list="false">
+            <i class='el-icon-plus'></i>
+        </el-upload>
+    </el-tab-pane>
   </el-tabs>
 </template>
 
@@ -34,6 +40,19 @@ export default {
     }
   },
   methods: {
+    // 上传
+    uploadImg (params) {
+      let data = new FormData() // 实例化对象
+      data.append('image', params.file) // 添加文件参数4
+      this.$axios({
+        url: '/user/images',
+        method: 'post',
+        data
+      }).then(result => {
+        // result.data.url
+        this.$emit('selectOneImg', result.data.url) // 自定义事件名这里不再强制小写
+      })
+    },
     clickImg (url) {
       // alert(url)
       // 把地址传出去 字符传值 $emit 第一个参数是事件名称（需小写），第二个参数是若干参数
@@ -77,5 +96,17 @@ export default {
       height: 100%;
     }
   }
+}
+.upload {
+   display: flex;
+   justify-content: center;
+    i {
+        font-size: 50px;
+        display: block;
+        margin: 20px auto;
+        padding: 60px;
+        border: 1px dashed #ccc;
+        border-radius: 4px;
+    }
 }
 </style>
