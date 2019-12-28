74b1af27452f30c463d4831d84c78e7be51067c0
gcy <gcy4399@163.com>
2019-12-28 22:03:40 +0800

    echarts在项目中的应用

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,6 @@ import SelectImg from './publish/select-image.vue'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
-
 export default {
   install (Vue) {
     Vue.component('layout-aside', LayoutAside)
